b654134b17ad3f2f51a88956d9c52074e980a88c
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-02-12 17:12:02 -0500

    Added incremenet_hunger as one of the included batteries.

---
+++ person_simulator/actions/increment_hunger.py
@@ -0,0 +1,12 @@
+from ..action import Action
+
+
+class IncrementHunger(Action):
+    def __init__(self, increment_amount=1, max_hunger=100):
+        super().__init__('IncrementHunger(+%d, max=%d)' % (increment_amount, max_hunger))
+        self._increment_amount = increment_amount
+        self._max_hunger = max_hunger
+
+    def __call__(self, actor, target):
+        target.hunger = min(target.hunger + self._increment_amount, self._max_hunger)
+        return target
